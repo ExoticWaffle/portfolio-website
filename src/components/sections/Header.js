c3ea9fd605fd5cfe9ea6bb86cc30edcbe6ee2431
Exz0tikWaffle <leotrem2567@gmail.com>
2023-05-08 13:41:49 -0400

    Improved responsiveness

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,11 +1,16 @@
 import { Box, Typography } from '@mui/material';
-import {React, forwardRef} from 'react';
+import {React, useState, forwardRef} from 'react';
 
 
 const Header = forwardRef((props, ref) => {
+  const [mobile, setMobile] = useState(window.innerWidth <= 640);
+  const checkMobile = () => {
+    setMobile(window.innerWidth <= 640);
+  }
+  window.addEventListener('resize', checkMobile);
     return (
         <div style={styles.container} ref={ref}>
-          <Typography variant="h1">Leo Tremblay</Typography>
+          <Typography variant={mobile ? "h3" : "h1"}>Leo Tremblay</Typography>
           <Typography>I'm a programmer</Typography>
         </div>
   );
